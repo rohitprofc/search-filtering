c3a4fc6fd6363867db6b73d52f9b563493496bbe
rohitprofc <rohitprofc@gmail.com>
2024-04-08 00:39:35 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from "react-bootstrap";
+import SearchInput from "./components/SearchInput";
+import SearchOutput from "./components/SearchOutput";
+
+const data = [
+  {
+    job_title: "Data Scientist",
+    name: "Rohit Kavuluri",
+  },
+  {
+    job_title: "Software Developer",
+    name: "Sowmya Sri Kancharla",
+  },
+  {
+    job_title: "AI Specialist",
+    name: "Priya Singh",
+  },
+  {
+    job_title: "Cybersecurity Specialist",
+    name: "Aakash Patel",
+  },
+  {
+    job_title: "Renewable Energy Engineer",
+    name: "Rani Kapoor",
+  },
+  {
+    job_title: "Surgeon",
+    name: "Dr. Vivek Joshi",
+  },
+  {
+    job_title: "Investment Banker",
+    name: "Meena Malhotra",
+  },
+  {
+    job_title: "Chief Executive Officer (CEO)",
+    name: "Arjun Desai",
+  },
+  {
+    job_title: "Anesthesiologist",
+    name: "Dr. Sunita Patel",
+  },
+  {
+    job_title: "Marketing Manager",
+    name: "Amitabh Singh",
+  },
+];
+
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState(""); // 1. State for searchTerm
+
+  const handleSearchChange = (newSearchTerm) => { // 2. Function to handle changes
+    setSearchTerm(newSearchTerm);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row className="justify-content-center">
+        <Col xs={10} lg={6}>
+          <h1 id="heading">Search Filtering</h1>
+          <SearchInput onSearchChange={handleSearchChange} /> {/* 3. Pass the function as a prop */}
+          <SearchOutput data={data} searchTerm={searchTerm} /> {/* 4. Pass searchTerm as a prop */}
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
